098a4dbf9a08df1d51573635915ba34883fef7cf
bkrishnasai1998 <59241777+bkrishnasai1998@users.noreply.github.com>
2022-11-01 20:54:46 -0500

    added images, selector.pug and js. Code changes on app.js file

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var onionRouter = require('./routes/onion');
 var gridRouter = require('./routes/gridbuild');
+var selectorRouter = require('./routes/selector');
 
 var app = express();
 
@@ -25,6 +26,7 @@ app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/onion',onionRouter);
 app.use('/gridbuild',gridRouter);
+app.use('/selector',selectorRouter);
 
 
 // catch 404 and forward to error handler
